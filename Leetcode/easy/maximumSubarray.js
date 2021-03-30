08cac631f8630ac07439370df09421b60918ebe7
Nahyeon Lee <naboee0627@gmail.com>
2021-03-30 21:35:45 +0300

    update Leetcode > easy

---
+++ Leetcode/easy/maximumSubarray.js
@@ -0,0 +1,18 @@
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) 
+ * which has the largest sum and return its sum.
+ * https://leetcode.com/problems/maximum-subarray/
+ */
+
+const maxSubArray = (nums) => {
+    const memo = [nums[0]];
+    let currMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        const subMax = Math.max(memo[i - 1] + nums[i], nums[i]);
+        memo.push(subMax);
+        currMax = Math.max(subMax, currMax);
+    }
+
+    return currMax;
+};
